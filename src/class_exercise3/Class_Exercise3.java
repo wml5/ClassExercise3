9a0480060efea9ceaa11ee8d168dfd0c39611f2a
wml5@students.ptcollege.edu <>
2018-10-22 15:33:10 -0400

    this is the first commit

---
+++ src/class_exercise3/Class_Exercise3.java
@@ -0,0 +1,20 @@
+/*
+ * wml5
+ * 10/22/2018
+ * Class Exercise 03
+ */
+package class_exercise3;
+
+public class Class_Exercise3 
+{
+
+    public static void main(String[] args) 
+    {
+        Ludwick();
+    }
+    
+    public static void Ludwick()
+    {
+        System.out.println("William Ludwick");
+    }
+}
